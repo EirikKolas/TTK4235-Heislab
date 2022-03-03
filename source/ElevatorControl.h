cf2a1f537447864c2ef0b7fbd5fe737b55a302d5
Herman <hhbale@stud.ntnu.no>
2022-03-03 11:11:37 +0100

    la til token kommentar

--- source/ElevatorControl.h
+++ source/ElevatorControl.h
@@ -6,3 +6,6 @@
 #pragma once
 void initElevator();
 void runElevator();
+
+
+//  vscode://vscode.github-authentication/did-authenticate?windowid=1&code=e51a12ef51088c2a2d23&state=3eed039a-0131-48b7-9a03-b6cdb0007f7c 
