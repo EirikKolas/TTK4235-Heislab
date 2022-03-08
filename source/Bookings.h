be073d9cf79516e0e3246eb33422b9073c2c5ec3
rebekalv <rebekk0607@hotmail.com>
2022-03-08 12:39:20 +0100

    Lagt inn bookings.h dokumentasjon

--- source/Bookings.h
+++ source/Bookings.h
@@ -8,8 +8,37 @@
 
 #define NO_BOOKINGS -1
 
+/**
+* @brief Makes sure there aren't any active bookings when the elevator is initialiazed.
+*/
 void initBookings();
+
+/**
+* @brief Clears all bookings at a specific floor.
+*
+* @param[in] floor Elevator floor where the bookings are to be cleared.
+*/
 void clearBooking(int floor);
+
+/**
+* @brief Checks all bookings at all floors.
+*/
 void clearAllBookings();
+
+/**
+* @brief Sets an active booking with a direction on a specific floor
+*
+* @param[in] floor Elevator floor where the booking is set.
+* @param[in] dir Direction of the booking.
+*/
 void setBooking(int floor, Direction dir);
+
+/**
+* @brief Calculates the next destination from all the active bookings based on the elevator's current floor and direction.
+*
+* @param[in] currentFloor Floor where the elevator is located.
+* @param[in] dir Direction of the elevator.
+*
+* @return the next floor
+*/
 int  getNextDestination(int currentFloor, Direction dir);
